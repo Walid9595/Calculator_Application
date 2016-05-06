5abf2e863dec8b89550ae01ebc773da4131c5345
Ben <Ben.Bahy@gmail.com>
2016-05-06 22:55:29 +0200

    Partial refactoring,new features ("sin cos tan" & hot keys) add.

--- src/main/java/com/gluonapplication/Buttons_Builder.java
+++ src/main/java/com/gluonapplication/Buttons_Builder.java
@@ -15,193 +15,155 @@ public class Buttons_Builder  {
 		tf.setEditable(false);
 		return tf;
 	}
-	
+	public void adjustButton(Button bt){
+        bt.prefWidthProperty().bind(GluonApplication.pane.widthProperty().divide(4));
+        bt.prefHeightProperty().bind(GluonApplication.pane.heightProperty().divide(4));
+        bt.setFont(Font.font(15));
+	}
 	public Button zero ()
 	{
 		Button bt0=new Button("0");
-		
-		bt0.setFont(Font.font(15));
-		//bt0.setStyle("-fx-background-color: rgb(184,184,184)");	
+		adjustButton(bt0);
 		return bt0;
 	}
 	
 	public Button one()
 	{
 		Button bt1=new Button("1");
-		
-		bt1.setFont(Font.font(15));
-		//bt1.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt1);
 		return bt1;
 	}
 	
 	public Button two()
 	{
 		Button bt2=new Button("2");
-		
-		bt2.setFont(Font.font(15));
-		//bt2.setStyle("-fx-background-color:rgb(184,184,184)");
+        adjustButton(bt2);
 		return bt2;
 	}
 	
 	public Button three()
 	{
 		Button bt3=new Button("3");
-		
-		bt3.setFont(Font.font(15));
-		//bt3.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt3);
 		return bt3;
 	}
 	
 	public Button four()
 	{
 		Button bt4=new Button("4");
-		
-		bt4.setFont(Font.font(15));
-		//bt4.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt4);
 		return bt4;
 	}
 	
 	public Button five()
 	{
 		Button bt5=new Button("5");
-		
-		bt5.setFont(Font.font(15));
-		//bt5.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt5);
 		return bt5;
 	}
 	
 	public Button six()
 	{
 		Button bt6=new Button("6");
-		
-		bt6.setFont(Font.font(15));
-		//bt6.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt6);
 		return bt6;
 	}
 	
 	public Button seven()
 	{
 		Button bt7=new Button("7");
-		
-		bt7.setFont(Font.font(15));
-		//bt7.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt7);
 		return bt7;
 	}
 	
 	public Button eight()
 	{
 		Button bt8=new Button("8");
-		
-		bt8.setFont(Font.font(15));
-		//bt8.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt8);
 		return bt8;
 	}
 	
 	public Button nine()
 	{
 		Button bt9=new Button("9");
-		
-		bt9.setFont(Font.font(15));
-		//bt9.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(bt9);
 		return bt9;
 	}
 	
    public Button add()
    {
 	   Button add=new Button("+");
-		
-		add.setFont(Font.font(15));
-		//add.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(add);
 		return add;
    }
    
    public Button sub()
    {
 	   Button sub=new Button("-");
-		
-		sub.setFont(Font.font(15));
-		//sub.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(sub);
 		return sub;
    }
    
    public Button mul()
    {
 	   Button mult=new Button("*");
-		
-		mult.setFont(Font.font(15));
-		//mult.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(mult);
 		return mult;
    }
    
    public Button div()
    {
 	   Button div=new Button("/");
-		
-		div.setFont(Font.font(15));
-		//div.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(div);
 		return div;
    }
    
    public Button equal()
    {
 	   Button equ=new Button("=");
-		
-		equ.setFont(Font.font(15));
-		//equ.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(equ);
 		return equ;
    }
    
    public Button leftPar()
    {
 	   Button par=new Button("(");
-		
-		par.setFont(Font.font(15));
-		//par.setStyle("-fx-background-color:rgb(184,184,184)");
+       adjustButton(par);
 		return par;
    }
    
    public Button rightPar()
    {
 	    Button par2=new Button(")");
-		
-		par2.setFont(Font.font(15));
-		//par2.setStyle("-fx-background-color: rgb(184,184,184)");
+        adjustButton(par2);
 		return par2;
    }
    
    public Button dot()
    {
 	   Button dot=new Button(".");
-		
-		dot.setFont(Font.font(15));
-		//dot.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(dot);
 		return dot;
    }
    
    public Button mod()
    {
 	   Button mod=new Button("%");
-		
-		mod.setFont(Font.font(15));
-		//mod.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(mod);
 		return mod;
    }
 
    public Button square()
    {
 	   Button sq=new Button("^2");
-		
-		sq.setFont(Font.font(15));
-		//sq.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(sq);
 		return sq;
    }
    
    public Button sqRoot()
    {
 	   Button sqrt=new Button("√");
-		
-		sqrt.setFont(Font.font(15));
-		//sqrt.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(sqrt);
 		return sqrt;
    }
    
@@ -211,19 +173,33 @@ public class Buttons_Builder  {
    public Button delete()
    {
 	   Button del=new Button("X");
-		
-		del.setFont(Font.font(15));
-		//del.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(del);
 		return del;
    }
    
    public Button clear()
    {
 	   Button cl=new Button("c");
-		
-		cl.setFont(Font.font(15));
-		//cl.setStyle("-fx-background-color: rgb(184,184,184)");
+       adjustButton(cl);
         return cl;
    }
+
+    public Button sin() {
+       Button sin=new Button("sin");        
+       adjustButton(sin);
+        return sin;
+    }
+    
+    public Button cos() {
+        Button cos=new Button("cos");        
+        adjustButton(cos);
+        return cos;
+    }
+    
+    public Button tan() {
+        Button tan=new Button("tan");        
+        adjustButton(tan);
+        return tan;
+    }
    
 }
--- src/main/java/com/gluonapplication/GluonApplication.java
+++ src/main/java/com/gluonapplication/GluonApplication.java
@@ -1,15 +1,14 @@
 package com.gluonapplication;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
+import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
@@ -18,7 +17,7 @@ public class GluonApplication extends Application {
     boolean addState,subState,divState,mulState,sqrState,precState,exState;
     boolean finish = false;
     BorderPane pane1=new BorderPane();
-    GridPane pane=new GridPane();
+    public static GridPane pane=new GridPane();
     int pnum;
         @Override
         public void start(Stage primaryStage)
@@ -33,114 +32,64 @@ public class GluonApplication extends Application {
             TextField tf=buttons.txtfield();
             tf.prefWidthProperty().bind(pane1.widthProperty().divide(4));
             Button bt0=buttons.zero();
-            bt0.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt0.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt1=buttons.one();
-            bt1.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt1.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt2=buttons.two();
-            bt2.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt2.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt3=buttons.three();
-            bt3.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt3.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt4=buttons.four();
-            bt4.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt4.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt5=buttons.five();
-            bt5.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt5.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt6=buttons.six();
-            bt6.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt6.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt7=buttons.seven();
-            bt7.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt7.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt8=buttons.eight();
-            bt8.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt8.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button bt9=buttons.nine();
-            bt9.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            bt9.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button add=buttons.add();
-            add.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            add.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button sub=buttons.sub();
-            sub.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            sub.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button mult=buttons.mul();
-            mult.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            mult.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button div=buttons.div();
-            div.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            div.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button equ=buttons.equal();
-            equ.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            equ.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button par1=buttons.leftPar();
-            par1.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            par1.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button par2=buttons.rightPar();
-            par2.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            par2.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button dot=buttons.dot();
-            dot.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            dot.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button mod=buttons.mod();
-            mod.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            mod.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button sq=buttons.square();
-            sq.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            sq.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button sqrt=buttons.sqRoot();
-            sqrt.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            sqrt.prefHeightProperty().bind(pane.heightProperty().divide(4));
-            
-            
             Button del=buttons.delete();
-            del.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            del.prefHeightProperty().bind(pane.heightProperty().divide(4));
             Button cl=buttons.clear();
-            cl.prefWidthProperty().bind(pane.widthProperty().divide(4));
-            cl.prefHeightProperty().bind(pane.heightProperty().divide(4));
-            
-
-            
+            Button sin=buttons.sin();
+            Button cos=buttons.cos();
+            Button tan=buttons.tan();
 
             pane.add(cl, 0, 0);
             pane.add(del, 1, 0);
             pane.add(par1, 2, 0);
             pane.add(par2, 3, 0);
-            
-                    
+
             pane.add(mod, 0, 1);
             pane.add(sqrt, 1, 1);
             pane.add(sq, 2, 1);
             pane.add(div, 3, 1);
-            
-            
-            pane.add(bt7, 0, 2);
-            pane.add(bt8,1, 2);
-            pane.add(bt9,2, 2);
+
+            pane.add(sin,0,2);
+            pane.add(cos,1, 2);
+            pane.add(tan,2,2);
             pane.add(mult,3, 2);
-            
-            
-            pane.add(bt4, 0, 3);
-            pane.add(bt5, 1, 3);
-            pane.add(bt6, 2, 3);
+
+            pane.add(bt7, 0, 3);
+            pane.add(bt8,1, 3);
+            pane.add(bt9,2, 3);
             pane.add(sub, 3, 3);
-            
-            
-            pane.add(bt1,0,4);
-            pane.add(bt2,1,4);
-            pane.add(bt3, 2,4);
+
+            pane.add(bt4, 0, 4);
+            pane.add(bt5, 1, 4);
+            pane.add(bt6, 2, 4);
             pane.add(add, 3,4);
-            
-            
-            
-            pane.add(bt0,0,5);
-            pane.add(dot,1, 5);
-            pane.add(equ,2,5);
+
+            pane.add(bt1,0,5);
+            pane.add(bt2,1,5);
+            pane.add(bt3, 2,5);
+
+            pane.add(bt0,0,6);
+            pane.add(dot,1, 6);
+            pane.add(equ,2,6);
      
             Buttons_Events event = new Buttons_Events();
             bt0.setOnAction(e->{
@@ -413,13 +362,76 @@ public class GluonApplication extends Application {
             System.out.println("wrong exp");
         }
     });
+    sin.setOnAction(e->{
+        try{
+        Double val=Double.parseDouble(tf.getText());       
+            tf.setText(Math.sin(val)+"");
+            finish=true;
+        }
+        catch(Exception ex)
+        {}
+    });
+    cos.setOnAction(e->{
+        try{
+        Double val=Double.parseDouble(tf.getText());       
+            tf.setText(Math.cos(val)+"");
+            finish=true;
+        }
+        catch(Exception ex)
+        {}
+    });
+    tan.setOnAction(e->{
+        try{
+        Double val=Double.parseDouble(tf.getText());       
+            tf.setText(Math.tan(val)+"");
+            finish=true;
+        }
+        catch(Exception ex)
+        {}
+    });
             pane1.setTop(tf);
             pane1.setCenter(pane);
-
             Scene scene = new Scene(pane1);
+
+            scene.addEventHandler(KeyEvent.KEY_RELEASED, e -> {
+                if (KeyCode.ESCAPE.equals(e.getCode())) 
+                    Platform.exit();
+                else if (KeyCode.ADD.equals(e.getCode())) 
+                    add.fire();
+                else if (KeyCode.SUBTRACT.equals(e.getCode())) 
+                    sub.fire();
+                else if (KeyCode.MULTIPLY.equals(e.getCode())) 
+                    mult.fire();
+                else if (KeyCode.DIVIDE.equals(e.getCode())) 
+                    div.fire();
+                else if (KeyCode.EQUALS.equals(e.getCode())) 
+                    equ.fire();
+                else if (KeyCode.ENTER.equals(e.getCode()))
+                    equ.fire();
+                else if (e.getText().contains("0")) 
+                    bt0.fire();
+                else if (e.getText().contains("1")) 
+                    bt1.fire();
+                else if (e.getText().contains("2")) 
+                    bt2.fire();
+                else if (e.getText().contains("3")) 
+                    bt3.fire();
+                else if (e.getText().contains("4")) 
+                    bt4.fire();
+                else if (e.getText().contains("5")) 
+                    bt5.fire();
+                else if (e.getText().contains("6")) 
+                    bt6.fire();
+                else if (e.getText().contains("7")) 
+                    bt7.fire();
+                else if (e.getText().contains("8")) 
+                    bt8.fire();
+                else if (e.getText().contains("9")) 
+                    bt9.fire();
+                });
+
             scene.getStylesheets().add("application.css");
             primaryStage.setTitle("Calculator");
-            primaryStage.setFullScreen(true);
             
             primaryStage.setScene(scene);
             primaryStage.show();
